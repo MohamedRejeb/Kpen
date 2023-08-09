e926e4efad926bfa0ff910bc499a4772d13ff07f
M0Coding <mohamedrejeb445@gmail.com>
2023-08-09 11:42:18 +0100

    Added MyFramework.framework

---
+++ kpen.framework/Headers/kpen.h
@@ -0,0 +1,25 @@
+//
+//  kpen.h
+//  kpen
+//
+//  Created by Mohamed Ben Rejeb on 8/8/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kpen.
+FOUNDATION_EXPORT double kpenVersionNumber;
+
+//! Project version string for kpen.
+FOUNDATION_EXPORT const unsigned char kpenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kpen/PublicHeader.h>
+
+#import <kpen/NSAppleEventDescriptorHelperCategory.h>
+#import <kpen/WacomTabletDriver.h>
+#import <kpen/DeviceTracker.h>
+#import <kpen/PressureWinController.h>
+#import <kpen/WTView.h>
+#import <kpen/TabletEvents.h>
+#import <kpen/TabletApplication.h>
+#import <kpen/Wacom.h>
